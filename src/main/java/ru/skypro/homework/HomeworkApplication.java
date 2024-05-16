4ef9cfd5ee70966224c32f73ab20196917ca51f8
korrova11 <korrova@yandex.ru>
2024-05-16 16:55:17 +0300

    add UserController(2 end-point) commit

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -1,11 +1,20 @@
 package ru.skypro.homework;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Documentation",
+                version = "1.0"
+
+        )
+)
 public class HomeworkApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(HomeworkApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkApplication.class, args);
+    }
 }
