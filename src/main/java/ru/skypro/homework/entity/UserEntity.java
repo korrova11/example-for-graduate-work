5ad89118d2ffe328970b726bc2bd2494e1a6f22c
korrova11 <korrova@yandex.ru>
2024-05-21 13:22:18 +0300

    add Doc at AuthController

---
+++ src/main/java/ru/skypro/homework/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package ru.skypro.homework.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.skypro.homework.dto.Image;
+import ru.skypro.homework.dto.Role;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class UserEntity {
+    @GeneratedValue
+    @Id
+    private Long id;
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+ //  private Image image;
+}
