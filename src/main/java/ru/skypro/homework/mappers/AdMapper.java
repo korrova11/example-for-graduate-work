9bc844a189902435e8b828dd5eb4f9c19a0e921c
vlj2007 <vlj2007@yandex.ru>
2024-05-23 21:38:41 +0700

    init commit

---
+++ src/main/java/ru/skypro/homework/mappers/AdMapper.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.mappers;
+
+
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface AdMapper {
+
+
+
+}
