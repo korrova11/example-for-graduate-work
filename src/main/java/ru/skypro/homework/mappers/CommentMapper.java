385c013ed9fad127e837215cdf0d9c2e6aff3f7c
VGlyva <135941893+VGlyva@users.noreply.github.com>
2024-05-23 21:32:15 +0700

    Создал маппер и тест CommentEntityToComment 1

--- src/main/java/ru/skypro/homework/mappers/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mappers/CommentMapper.java
@@ -7,7 +7,7 @@ import ru.skypro.homework.dto.Comment;
 import ru.skypro.homework.entity.CommentEntity;
 
 @Mapper
-public interface CommentMapper {
+public interface  CommentMapper {
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
     @Mapping(source = "id", target = "pk")
     Comment commentEntityToComment(CommentEntity commentEntity);
